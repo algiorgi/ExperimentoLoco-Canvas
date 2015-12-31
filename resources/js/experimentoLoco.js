c5c1d8da586d9202c39090d070140095f9a15375
geagiorgi <alejandro.giorgi@grupoesfera.com.ar>
2015-12-31 19:59:22 -0300

    Dibujando un circulo

--- resources/js/experimentoLoco.js
+++ resources/js/experimentoLoco.js
@@ -1,1 +1,11 @@
-console.log("PROBANDO...");	
+//VARIABLES
+var canvas, contexto;
+
+canvas = document.getElementById( 'canvasId' );
+contexto =  canvas.getContext( '2d' );
+
+contexto.beginPath();
+contexto.arc(canvas.width / 2, canvas.height / 2, 3, 0 , 2 * Math.PI);
+contexto.fillStyle = 'black';
+contexto.fill();
+	
