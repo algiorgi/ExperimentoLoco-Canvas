5dd9d8647b3e7ef488d5b9a6b7fd3c0d1167abc9
geagiorgi <alejandro.giorgi@grupoesfera.com.ar>
2015-12-31 20:08:44 -0300

    Clase copo. Funcion para dibujar un copo

--- resources/js/experimentoLoco.js
+++ resources/js/experimentoLoco.js
@@ -1,11 +1,36 @@
+//OBJETOS
+function Copo(radio, color) {
+	
+	//ATRIBUTOS
+	this.radio = radio;
+	this.color = color; 
+}
+
+
 //VARIABLES
-var canvas, contexto;
+var canvas, contexto, copo;
 
 canvas = document.getElementById( 'canvasId' );
 contexto =  canvas.getContext( '2d' );
 
-contexto.beginPath();
-contexto.arc(canvas.width / 2, canvas.height / 2, 3, 0 , 2 * Math.PI);
-contexto.fillStyle = 'black';
-contexto.fill();
+copo = new Copo(3, 'black');
+copo.x = 10;
+copo.y = 10;
+
+dibujar(copo);	
+
+otroCopo = new Copo(5, 'blue');
+otroCopo.x = 20;
+otroCopo.y = 10;
+
+dibujar(otroCopo);
+
+//FUNCIONES
+function dibujar(copo) {
+		
+	contexto.beginPath();
+	contexto.arc(copo.x, copo.y, copo.radio, 0 , 2 * Math.PI);
+	contexto.fillStyle = copo.color;
+	contexto.fill();	
 	
+} 	
